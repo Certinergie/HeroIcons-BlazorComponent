81c7a23eac8ea3bef7f2479686e82732f3833c3c
Samuel <samuel@certinergie.be>
2021-10-08 09:03:40 +0200

    :fire: Remove boilerplat code +

--- src/Heroi-Icons/Heroi-Icons/Startup.cs
+++ src/Heroi-Icons/Heroi-Icons/Startup.cs
@@ -1,17 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Heroi_Icons.Data;
 
-namespace Heroi_Icons
+namespace HeroIcons.BlazorComponent
 {
     public class Startup
     {
@@ -28,7 +21,6 @@ namespace Heroi_Icons
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
