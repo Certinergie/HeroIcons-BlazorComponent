82cb9857c35158f0142ce436159af0ecdf56df9d
Hendrick_Samuel <57829885+HendrickSamuel@users.noreply.github.com>
2021-10-11 21:54:40 +0200

    Add outlined circle arrows + simple arrows (#4)
    Co-authored-by: Samuel <samuel@certinergie.be>

--- src/HeroIcons.BlazorComponent.Demo/Pages/Index.razor.cs
+++ src/HeroIcons.BlazorComponent.Demo/Pages/Index.razor.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections.Generic;
+using HeroIcons.BlazorComponent.Demo.Services;
 using HeroIcons.BlazorComponent.Models;
+using Microsoft.AspNetCore.Components;
 
 namespace HeroIcons.BlazorComponent.Demo.Pages
 {
     public partial class Index
     {
+        [Inject]
+        public ClipboardService ClipboardService { get; set; }
+        
         public Dictionary<string, IconType> Icons { get; set; }
+        
+        private void Callback(IconType iconType)
+        {
+            ClipboardService.WriteTextAsync($"<IconComponent IconType=\"IconType.{iconType}\"/>");
+        }
+        
 
         protected override void OnInitialized()
         {
@@ -16,6 +27,13 @@ namespace HeroIcons.BlazorComponent.Demo.Pages
                 { "annotation", IconType.Annotation },
                 { "archive", IconType.Archive },
                 { "arrow-circle-down", IconType.ArrowCircleDown },
+                { "arrow-circle-left", IconType.ArrowCircleLeft },
+                { "arrow-circle-right", IconType.ArrowCircleRight },
+                { "arrow-circle-up", IconType.ArrowCircleUp },
+                { "arrow-down", IconType.ArrowDown },
+                { "arrow-left", IconType.ArrowLeft },
+                { "arrow-right", IconType.ArrowRight },
+                { "arrow-up", IconType.ArrowUp },
             };
         }
     }
